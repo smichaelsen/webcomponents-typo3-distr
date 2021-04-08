da961ba0bd2a7535e29a9cdd32c1417d04ac6103
Matthias Stegmann <matthias.stegmann@b13.com>
2021-04-08 11:06:00 +0200

    [TASK] Fix image processing for local development
    and other TYPO3_CONF_VARS
    
    #29

--- config/AdditionalConfiguration/Development/DDEVContext.php
+++ config/AdditionalConfiguration/Development/DDEVContext.php
@@ -1,12 +1,46 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = [
-    'charset' => 'utf8',
-    'dbname' => 'db',
-    'host' => 'db',
-    'password' => 'db',
-    'port' => 3306,
-    'user' => 'db',
-    'driver' => 'mysqli',
-];
+// base config by DDEV
+if (getenv('IS_DDEV_PROJECT') == 'true') {
+    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+        $GLOBALS['TYPO3_CONF_VARS'],
+        [
+            'DB' => [
+                'Connections' => [
+                    'Default' => [
+                        'charset' => 'utf8',
+                        'dbname' => 'db',
+                        'host' => 'db',
+                        'password' => 'db',
+                        'port' => 3306,
+                        'user' => 'db',
+                        'driver' => 'mysqli',
+                    ],
+                ],
+            ],
+            // This GFX configuration allows processing by installed ImageMagick 6
+            'GFX' => [
+                'processor' => 'ImageMagick',
+                'processor_path' => '/usr/bin/',
+                'processor_path_lzw' => '/usr/bin/',
+            ],
+            // This mail configuration sends all emails to mailhog
+            'MAIL' => [
+                'transport' => 'smtp',
+                'transport_smtp_server' => 'localhost:1025',
+            ],
+            'SYS' => [
+                'trustedHostsPattern' => '.*',
+                'devIPmask' => '*',
+                'displayErrors' => 1,
+            ],
+            'BE' => [
+                'debug' => true,
+            ],
+            'FE' => [
+                'debug' => true,
+            ]
+        ]
+    );
+}
+
