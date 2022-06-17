86698bc48e74b18df9011f776fc9a7b1aa878f45
Ruben Steeb <r.steeb@imia.de>
2022-06-17 09:56:15 +0200

    [TASK] Add GitLab information Widget

---
+++ src/extensions/site_t3demo/Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'default' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:dashboard.default',
+        'description' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:dashboard.default.description',
+        'iconIdentifier' => 'content-dashboard',
+        'defaultWidgets' => ['t3information', 'docGettingStarted', 't3demoGitlab'],
+        'showInWizard' => false,
+    ],
+];
